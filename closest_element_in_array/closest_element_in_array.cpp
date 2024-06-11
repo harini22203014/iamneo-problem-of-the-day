b0fa4ddcf609e32d6570d37cbe1ed770fea5679c
harini22203014 <119642200+harini22203014@users.noreply.github.com>
2024-06-11 22:44:48 +0530

    Create closest_element_in_array.cpp

---
+++ closest_element_in_array/closest_element_in_array.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int closest(int arr[], int s,int b){
+int diff=101;
+int num=0;
+for(int i=0;i<s;i++){
+if(arr[i]==b){
+return b;
+}
+else if(arr[i]<b && (b-arr[i])<diff){
+diff-b-arr[i];
+num=arr[i];
+}
+}
+return num;
+}
+int main(){
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+cin>>arr[i];
+}
+int h;
+cin>>h;
+int res=closest(arr,n,h);
+if(res>0){
+cout<<"The closest item ID less than or equal to "<<h<<" is "<<res;
+}
+else{
+cout<<"No closest item with an ID less than or equal to "<<h<<" exists in the wardobe";
+}
+}
